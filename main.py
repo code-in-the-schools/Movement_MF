d0fb0552a067f792ba26184184746d93877c6a76
MyronFields1 <68288662+MyronFields@users.noreply.github.com>
2020-07-31 17:37:33 +0000

    pong

--- main.py
+++ main.py
@@ -1,51 +1,125 @@
 import pygame
-import os
+import random
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+ 
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 500
+BALL_SIZE = 25
+ 
+ 
+class Ball:
+    """
+    Class to keep track of a ball's location and vector.
+    """
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.change_x = 0
+        self.change_y = 0
+ 
+ 
+def make_ball():
+    """
+    Function to make a new, random ball.
+    """
+    ball = Ball()
+    ball.x = random.randrange(BALL_SIZE, SCREEN_WIDTH - BALL_SIZE)
+    ball.y = random.randrange(BALL_SIZE, SCREEN_HEIGHT - BALL_SIZE)
+ 
+    # Speed and direction of rectangle
+    ball.change_x = random.randrange(-2, 3)
+    ball.change_y = random.randrange(-2, 3)
+ 
+    return ball
+ 
+ 
+def main():
+    """
+    This is our main program.
+    """
+    pygame.init()
+ 
+    # Set the height and width of the screen
+    size = [SCREEN_WIDTH, SCREEN_HEIGHT]
+    screen = pygame.display.set_mode(size)
+ 
+    pygame.display.set_caption("Bouncing Balls")
+ 
+    # Loop until the user clicks the close button.
+    done = False
+ 
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+ 
+    ball_list = []
+ 
+    ball = make_ball()
+    ball_list.append(ball)
+ 
+    # -------- Main Program Loop -----------
+    while not done:
+        # --- Event Processing
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            elif event.type == pygame.KEYDOWN:
+                # Space bar! Spawn a new ball.
+                if event.key == pygame.K_SPACE:
+                    ball = make_ball()
+                    ball_list.append(ball)
+ 
+        # --- Logic
+        for ball in ball_list:
+            # Move the ball's center
+            ball.x += ball.change_x
+            ball.y += ball.change_y
+ 
+            # Bounce the ball if needed
+            if ball.y > SCREEN_HEIGHT - BALL_SIZE or ball.y < BALL_SIZE:
+                ball.change_y *= -1
+            if ball.x > SCREEN_WIDTH - BALL_SIZE or ball.x < BALL_SIZE:
+                ball.change_x *= -1
+ 
+        # --- Drawing
+        # Set the screen background
+        screen.fill(BLACK)
+ 
+        # Draw the balls
+        for ball in ball_list:
+            pygame.draw.circle(screen, WHITE, [ball.x, ball.y], BALL_SIZE)
+ 
+        # --- Wrap-up
+        # Limit to 60 frames per second
+        clock.tick(60)
+ 
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+ 
+    # Close everything down
+    pygame.quit()
+ 
+if __name__ == "__main__":
+    main()
+ def main(): 
+   pygame.display.set_caption('Pong')
+	 screen = pygame.display.set_mode((WIN_W, WIN_H), pygame.SRCALPHA)
+	
+	paddle_width = 15
+	paddle_height = 100
+	
+	paddle = pygame.Surface((paddle_width, paddle_height))
+	
+	clock = pygame.time.Clock()
+	play = True
+	screen.fill(WHITE)
 
-img_path = os.path.join("ZeldaLink.png")
-
-class character(object):
-  def __init__(self):
-    pygame.sprite.Sprite.__init__(self)
-
-    character.image = pygame.image.load("ZeldaLink.png")
-    self.image = character.image
-    self.image = pygame.transform.scale(self.image(50,50))
-
-    self.x = 50
-    self.y = 50
-    self.hitbox = (self.x,self.y, 55, 55)
-
-def draw(self, surface):
-  surface.blit(self.image,(self.x, self.y))
-
-def movement(self):
-  key = pygame.key.get_pressed()
-
-  if key[pygame.K_DOWN]:
-    self.y += 1
-
-  pygame.init()
-  screen_width = 600
-  screen_height = 600
-  screen = pygame.display.set_mode((screen_width, screen_height))
-
-  Sprite = character()
-  clock = pygame.time.Clock()
-
-  running = True
-  while running:
-
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        pygame.quit()
-        running = False
+	while play:
+	 	pygame.event.get()
+	 	
+		clock.tick
+		pygame.display.flip()	
 
 
 
-
-        screen.fill(255,255,255)
-        Sprite.draw(screen)
-        pygame.display.update()
-
-
-        clock.tick(60) #clock
+#http://programarcadegames.com/python_examples/f.php?file=bouncing_balls 
